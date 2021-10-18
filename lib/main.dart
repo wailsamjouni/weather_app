f6140883897b2c8d394c92d5f4d1e833a8421c77
wailsamjouni <51680878+wailsamjouni@users.noreply.github.com>
2021-10-19 00:01:25 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/widgets/appbar_widget.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final double heightOfScreen = MediaQuery.of(context).size.height;
+    final double widthOfScreen = MediaQuery.of(context).size.width;
+    return Stack(
+      children: <Widget>[
+        Positioned(
+            bottom: heightOfScreen / 2.3,
+            child: Image.network(
+              'https://i.ibb.co/Y2CNM8V/new-york.jpg',
+              height: heightOfScreen,
+            )),
+        Positioned(
+            bottom: 0.0,
+            child: Container(
+              color: Color(0xff38383d),
+              height: heightOfScreen / 2.3,
+              width: widthOfScreen,
+            )),
+        AppBarWidget(),
+      ],
+    );
+  }
+}
---
+++ lib/widgets/appbar_widget.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends StatelessWidget {
+  const AppBarWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black54,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            size: 30.0,
+          ),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.access_alarm,
+                      size: 30.0,
+                    )),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.more_horiz,
+                      size: 30.0,
+                    )),
+              ],
+            ),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 60),
+            Text(
+              'How is the weather',
+              style: TextStyle(
+                  fontSize: 35.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Search your city',
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
